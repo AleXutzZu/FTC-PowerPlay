c7bde51f2aaff4f1782631ee04bd11c829a0c08c
AleXutzZu <alex.david@eminescusm.ro>
2023-01-06 19:53:49 +0200

    Added an OpMode to test for the encoder output on an individual motor (named "motor" in the configuration) and it outputs the direction relative to the observer

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/test/TestMotor.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop.test;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Test Motor", group = "Debugging")
+public class TestMotor extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "motor");
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        GamepadEx gamepad = new GamepadEx(gamepad1);
+        ToggleButtonReader switchDirection = new ToggleButtonReader(gamepad, GamepadKeys.Button.X);
+        boolean direction = true;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Change Direction", "Press X");
+            if (switchDirection.getState()) {
+                direction = !direction;
+            }
+
+            if (direction) motor.setDirection(DcMotorSimple.Direction.FORWARD);
+            else motor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            double power = -gamepad1.left_stick_y;
+
+            motor.setPower(power);
+
+            telemetry.addData("Encoder count", motor.getCurrentPosition());
+
+            String directionString;
+
+            if (direction && power > 0) directionString = "FORWARD";
+            else if (direction && power < 0) directionString = "REVERSE";
+            else if (!direction && power > 0) directionString = "REVERSE";
+            else if (!direction && power < 0) directionString = "FORWARD";
+            else directionString = "STOPPED";
+
+            telemetry.addData("Direction", "%s (act. %s)", motor.getDirection(), directionString);
+            telemetry.update();
+            switchDirection.readValue();
+        }
+    }
+}
