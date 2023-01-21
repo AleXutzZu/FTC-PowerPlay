beb5ea1ad34fe7ddc21c6c6b2149fe3b98dff4cd
AleXutzZu <alex.david@eminescusm.ro>
2023-01-21 10:25:33 +0200

    Added a Claw Test OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/test/TestClaw.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop.test;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
+
+@TeleOp(name = "Test Claw", group = "Debugging")
+public class TestClaw extends LinearOpMode {
+    private final RobotHardware robotHardware = new RobotHardware(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robotHardware.initDrivetrainMotors();
+        GamepadEx gamepad = new GamepadEx(gamepad1);
+        ToggleButtonReader toggleClaws = new ToggleButtonReader(gamepad, GamepadKeys.Button.X);
+
+        telemetry.addData(">", "Start OpMode");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            telemetry.addData(">", "Press X to toggle claws");
+            telemetry.addData("State", toggleClaws.getState());
+
+            if (gamepad.wasJustReleased(GamepadKeys.Button.X)) {
+                boolean state = toggleClaws.getState();
+                if (state) {
+                    robotHardware.getLeftClawServo().setPosition(0);
+                    robotHardware.getRightClawServo().setPosition(0);
+                } else {
+                    robotHardware.getLeftClawServo().setPosition(1);
+                    robotHardware.getRightClawServo().setPosition(1);
+                }
+            }
+
+            telemetry.update();
+            toggleClaws.readValue();
+            gamepad.readButtons();
+        }
+    }
+}
