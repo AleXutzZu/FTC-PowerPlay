d0e55c0bdd09d0e8211fc86abfb237c056dc5c53
AleXutzZu <alex.david@eminescusm.ro>
2023-01-26 14:59:41 +0200

    Finished OmniMovement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/movement/OmniMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/movement/OmniMovement.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop.movement;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.control.TeleOpControl;
 
-//TODO: Fix up this to use the new TeleOpControl class
+@TeleOp(name = "Omni Movement", group = "Movement")
 public class OmniMovement extends TeleOpControl {
     @Override
     protected void run() {
